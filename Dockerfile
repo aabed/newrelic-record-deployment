48e038bd19e46939a2512f3c8873b771f5673a32
Aabed <ahmad.aabed.m@gmail.com>
2021-09-17 22:14:54 +0200

    fixes newrelic template path

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 # Container image that runs your code
 FROM python:alpine
+WORKDIR /
 RUN pip install newrelic
 COPY entrypoint.sh /entrypoint.sh
 COPY newrelic.ini.tmpl /newrelic.ini.tmpl
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
-envsubst <newrelic.ini.tmpl>newrelic.ini
+envsubst < ./newrelic.ini.tmpl > newrelic.ini
 newrelic-admin record-deploy newrelic.ini $DEPLOYMENT_DESCRIPTION $DEPLOYMENT_REVISION $DEPLOYMENT_CHANGELOG $DEPLOYMENT_USER
