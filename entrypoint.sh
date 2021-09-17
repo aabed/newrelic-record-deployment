5c9dddbbf69d9e312a00dd809bfc80a2a9a37c5a
Aabed <ahmad.aabed.m@gmail.com>
2021-09-17 22:25:50 +0200

    testing file not found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
-envsubst < ./newrelic.ini.tmpl > newrelic.ini
+ls
+pwd
+envsubst < newrelic.ini.tmpl > newrelic.ini
 newrelic-admin record-deploy newrelic.ini $DEPLOYMENT_DESCRIPTION $DEPLOYMENT_REVISION $DEPLOYMENT_CHANGELOG $DEPLOYMENT_USER
